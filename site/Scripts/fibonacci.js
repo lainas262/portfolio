0ef75e2533067188cddf2c0036707438eeab9880
lainas262 <lainas262@gmail.com>
2016-03-07 01:24:17 +0000

    Circle Animation Complete!  -Next Goal to change animation speed for each circle   as its approaching the ends.

--- site/Scripts/fibonacci.js
+++ site/Scripts/fibonacci.js
@@ -1,83 +1,39 @@
- function fibonacci() {
-    var a = 1;
-    console.log(a);
-    var b = 1;
-    console.log(b);
-    
-    var para = document.createElement("p");
-    var node = document.createTextNode(a);    
-    para.appendChild(node);
-    var element = document.getElementById("fibonacciCounter");
-    element.appendChild(para);
-    
-    para = document.createElement("p");
-    node = document.createTextNode(b);
-    para.appendChild(node);
-    
-    element = document.getElementById("fibonacciCounter");
-    element.appendChild(para);
-    
-    var c = 0;
-    while (a<100000000000){
-        c = a+b;
-        
-        para = document.createElement("p");
-        node = document.createTextNode(c);
-        para.appendChild(node);
-    
-        element = document.getElementById("fibonacciCounter");
-        element.appendChild(para);
-        
-        a=b;
-        b = c;
-    }
-}
+/*
+ *  Author: Emmanuel Lainas
+ *  Exploring the canvas element with a simple animation
+ */
 
+//Global Variables
 
-function fibonacci2() {
-    var a = 1;
-    var b = 1;
-    var c = 0;
-    while (a<100000000000){
-        c = a+b;    
-        a=b;
-        b = c;
-    }
-}
-
-//above not needed
-
-//global vars
-
+//circles array to hold circle objects
 var circles = new Array();
-
+//geting the canvas html element so that we can draw on it use Javascript 
 var canvas = document.getElementById("myCanvas");
+//initializing canvas context
 var ctx = canvas.getContext("2d");
+//Calculate the X and Y center of canvas
 var centerX = canvas.width / 2;
 var centerY = canvas.height / 2;
-//var arcAnimationIncreament = 0.01*Math.PI;
-var finalEndingAngle = 0;
-var finalStartingAngle = 0;
-//var startingAngle = 0*Math.PI;
-//var endingAngle = 2*Math.PI;
-//var endingAngleIncrements = 0;
-//var startingAngleDicrements = 0;
-//var carriedArcSize = 0;
+//calculating canvas diagonal from center using "centerX" and "centerY" 
+//to get half the size of the actual canvas diagonal so that we can use
+//that value as the radius for the largest circle to be drawn from the center.
+//This is beneficial when the canvas is resized as only the amount of circles
+//necessary to fit the canvas will be drawn. NOTE: If we want to fill the 
+//canvas with as many circles as possible centerX must be used when 
+//initializing the array circles. canvasDiagonalFromCenter can be used if we want
+//to fill the blank edges of the canvas whith more circles so that there are no
+//blank edges but instead arcs of larger circles.
 var canvasDiagonalFromCenter = Math.sqrt((Math.pow(centerX,2)+Math.pow(centerY,2)));
-//console.log(canvasDiagonalFromCenter);
-//console.log("ha");
+//initialize requestAnimationFrame based on browser (no callback function provided yet)
 var requestAnimationFrame = window.requestAnimationFrame || 
                             window.mozRequestAnimationFrame || 
                             window.webkitRequestAnimationFrame || 
                             window.msRequestAnimationFrame;
-
-
-var time;
-var dt;
-//var smallDt;
-var animate = true;
+//speed in this case is essentially the angle to increment each circle at each frame drawn
 var speed = 0.013*Math.PI;
+//delayIncreaments controls when inner circles are drawn
 var delayIncrements = 10;
+//Circle objects constructor
 function Circle(radius, startingAngle, endingAngle, positiveIncrement, negativeIncrement, delay){
     this.radius = radius;
     this.startingAngle = startingAngle;
@@ -86,70 +42,9 @@ function Circle(radius, startingAngle, endingAngle, positiveIncrement, negativeI
     this.negativeIncrement = this.endingAngle-speed;
     this.delay = delay;
 }
-
-Circle.prototype.updateClockwise = function(){
-//    if(this.positiveDelay > 0){
- //       this.positiveDelay-=1000
-  //      console.log("do i ever come here??");
-//        console.log(this);
-//    }
-//    else{
-        this.endingAngleIncrement = this.startingAngle+this.arcAnimationIncreament; 
-        ctx.beginPath();
-        ctx.arc(centerX,centerY,this.radius,this.startingAngle,this.endingAngleIncrement);
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = 'red';
-        ctx.stroke();
-        ctx.closePath();
-  //      console.log("test1");
-
- //       console.log(this.arcAnimationIncreament);
-        this.arcAnimationIncreament += 0.003*Math.PI;
- //       console.log("test1");
-        if(this.endingAngleIncrement>this.endingAngle){
-     //       animate=false;
-            
-            this.arcAnimationIncreament = 0;
-            this.startingAngleDecrement=0;
-            this.updateAntiClockwise();
-        }
-//    }
-   
- //       console.log(this);
-        
-    
-}
-
-Circle.prototype.updateAntiClockwise = function(){
- //   if(this.negativeDelay < 0){
- //       this.negativeDelay+=1000
-   //     console.log("do i ever come here??");
-    //    console.log(this);
- //   }
- //   else{
-    console.log("test1");
-        this.startingAngleDecrement = this.endingAngle-this.arcAnimationIncreament;
-        ctx.beginPath();
-        ctx.arc(centerX,centerY,this.radius,this.startingAngleDecrement,this.endingAngle);
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = 'green';
-        ctx.stroke();
-        ctx.closePath();
-        this.arcAnimationIncreament += 0.003*Math.PI;
-   //     console.log("test2");
-        console.log(this.startingAngleDecrement);
-        console.log(this.startingAngle);
-        if(this.startingAngleDecrement>this.startingAngle){
-            console.log("booooooobooooooo");
-   //         animate=false;
-            this.arcAnimationIncreament = 0;
-            this.endingAngleIncrement=0;
-            this.updateClockwise();
-        }
- //   }
-}
-
-Circle.prototype.updateCircle = function(){
+//Circle Animation logic. Start drawing circle clockwise increasing the size by "speed".
+//If the drawing increment has reached the end, start drawing the circle by subtracting "speed" from drawing increment. 
+Circle.prototype.animateClockwiseCircle = function(){
     if(this.positiveIncrement>this.endingAngle){
         ctx.beginPath();
         ctx.arc(centerX,centerY,this.radius,this.startingAngle,this.negativeIncrement);
@@ -171,144 +66,86 @@ Circle.prototype.updateCircle = function(){
         ctx.stroke();
         ctx.closePath();
         this.positiveIncrement += speed;
-     //   if(this.positiveIncrement>this.startingAngle){
-    //        this.negativeIncrement=0;
-      //  }
     }
 }
-
-
+//Fill circles array with as many circles as possible given the centerX
+//which will be used as the radius of the largest circle and decreasing 
+//it by 10 at each iteration. Additianally the delay value of each circle is 
+//increased at each iteration by "delayIncrements" in order to load inner circles with delay.
+//NOTE: We can replace centerX in the for loop with canvasDiagonalFromCenter
+//to fill the blank edges of the canvas with circle arcs that expand beyond the canvas.
 function setupCircles(){
- //   var positiveDelay = 1;
-//    var negativeDelay = 0;
- //   time = now;
+
     var delay=0;
-    
     for(var radius=centerX; radius>0; radius-=10){
         var circle = new Circle(radius,0.5*Math.PI,2.5*Math.PI,0,0,delay);
         circles.push(circle);
         delay+=delayIncrements;
     }
-    
-    console.log(circles[0]);
-    console.log(circles[1]);
-    console.log(circles[2]);
-    console.log(circles[3]);
-    console.log(circles[4]);
-    
     drawAndUpdateCircles();
 }
+//function to draw circles. Each circle is drawn if its delay value is
+//equal to zero. If not, delay is decreased by one. This is were the "delayIncrements"
+//variable comes in the picture. The higher the delayIncrements value, the more 
+//"requestAnimationFrame" will be called in order for an inner circle to reach a delay==0
+//which results in the outer circle getting drawn first 
+function drawAndUpdateCircles(){
+    //clear canvas at each frame
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 
+    for(var i=0; i<circles.length; i++){
+        if(circles[i].delay==0)
+            circles[i].animateClockwiseCircle();    
+        else
+            circles[i].delay-=1;
+    }
+    requestAnimationFrame(drawAndUpdateCircles);
+}
+//function call to start setting up circles and then animate
 setupCircles();
+//function call to the fibonacci function defined below
 fibonacci();
 
-function drawAndUpdateCircles(){
-    var now = new Date().getTime();
-    dt = (now - (time || now));
-    time = now;
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-         
-  //  myCircle.updateAntiClockwise();
-    for(var i=0; i<circles.length; i++){
-        myCircle = circles[i];
-        if(myCircle.delay==0)
-            myCircle.updateCircle();    
-        else
-            myCircle.delay-=1;
-    }
+
+
+/**************************************************************************************************/
+
+
+//EXTRA STUFF
+
+//A simple fibonacci counter
+//The purpose of this is to investigate dynamic loading of HTML nodes,
+//in this case paragraph nodes with the value of a fibonacci number as soon as its calculated
+function fibonacci() {
+    var a = 0;
+    var b = 1;
+    //create paragraph element
+    var para = document.createElement("p");
+    //create a text node with the value of variable "a"
+    var node = document.createTextNode(a);
+    //append the text node as a child to the paragraph element
+    para.appendChild(node);
+    //get the element with id "fibonacciCounter"
+    var element = document.getElementById("fibonacciCounter");
+    //append the paragraph element to the element
+    element.appendChild(para);
+    para = document.createElement("p");
+    node = document.createTextNode(b);
+    para.appendChild(node);
+    element = document.getElementById("fibonacciCounter");
+    element.appendChild(para);
+    var c = 0;
     
-    
-    //May need this
-/*    var positiveDelay = 0;
-    var negativeDelay = 0;
-    for(var i=0; i<circles.length; i++){
-        positiveDelay+=1000;
-        negativeDelay-=1000;
-        myCircle.positiveDelay = positiveDelay;
-        myCircle.negativeDelay = negativeDelay;
-    }
-*/
-    
-    if(animate==true){
-    requestAnimationFrame(drawAndUpdateCircles);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// trial Methods below not being used
-
-function drawClockwiseCircle(){
-    var now = new Date().getTime(),
-    dt = now - (time || now);
-    time = now;
-    ctx.clearRect(0,0,1300,1000);
-    finalEndingAngle = endingAngle;
-    endingAngleIncreaments = startingAngle+arcAnimationIncreament;
-        
-    ctx.beginPath();
-    ctx.arc(centerX,centerY,10,startingAngle,endingAngleIncreaments);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
-    ctx.stroke();
-    ctx.closePath();
-
-    ctx.beginPath();
-    ctx.arc(centerX,centerY,30,startingAngle,endingAngleIncreaments);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'green';
-    ctx.stroke();
-    ctx.closePath();
-
-    arcAnimationIncreament += (dt/500)*Math.PI;
-    console.log("test1");
-    if(endingAngleIncreaments<finalEndingAngle){
-        requestAnimationFrame(drawClockwiseCircle);
+    while (a<1000){
+        c = a+b;
+        para = document.createElement("p");
+        node = document.createTextNode(c);
+        para.appendChild(node);
+        element = document.getElementById("fibonacciCounter");
+        element.appendChild(para);
+        a=b;
+        b = c;
     }
 }
-
-function drawCounterClockwiseCircle(){
-    var now = new Date().getTime(),
-    dt = now - (time || now);
-    time = now;
-    ctx.clearRect(0,0,1300,1000);
-    finalStartingAngle = startingAngle;
-    startingAngleDicrements = endingAngle-arcAnimationIncreament;
-    console.log(startingAngleDicrements);
-    
-    ctx.beginPath();
-    ctx.arc(centerX,centerY,20,startingAngleDicrements,endingAngle);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
-    ctx.stroke();
-    ctx.closePath();
-    
-    ctx.beginPath();
-    ctx.arc(centerX,centerY,400,startingAngleDicrements,endingAngle);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'green';
-    ctx.stroke();
-    ctx.closePath();
-    
-    arcAnimationIncreament += (dt/500)*Math.PI;
-    console.log("test1");
-    if(startingAngleDicrements>finalStartingAngle){
-        requestAnimationFrame(drawCounterClockwiseCircle);
-    }
-}
-
 
 
