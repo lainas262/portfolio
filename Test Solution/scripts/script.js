a5e380b28cb0a896c84482d6f67cf7c8f445dd9c
lainas262 <lainas262@gmail.com>
2016-03-25 14:55:25 +0000

    Created button transition style when clicked and unclicked

--- Test Solution/scripts/script.js
+++ Test Solution/scripts/script.js
@@ -3,11 +3,12 @@ var hidden = true;
 function showArticles(){
     if(hidden == true){
         document.getElementById("hidden-articles").style.display ="block";
+        document.getElementById("article-button").className ="button clicked-button center-block";
         hidden = false;
     }
     else{
         document.getElementById("hidden-articles").style.display ="none";
-        document.getElementById("article-button").className ="btn btn-default center-block";
+        document.getElementById("article-button").className ="button unclicked-button center-block";
         hidden = true;
     }
 }
